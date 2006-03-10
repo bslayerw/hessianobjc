637e97db09239f7674e05a6c9dc63b04cc99f1ef
Byron Wright <bslayerw@gmail.com>
2006-03-10 00:11:21 +0000

    Added documentation

--- BBSHessianCall.h
+++ BBSHessianCall.h
@@ -16,18 +16,46 @@
 // limitations under the License.
 //
 
+/** Encapsulates a Hessian call. BBSHessianCall is responsible
+  * for encoding a Hessian call and all it's parameters.
+  */
+
 @interface BBSHessianCall : NSObject {
+    @private
     NSMutableData * callData;
     NSString * methodName;
     NSArray * parameters;
 }
 
+/** initialize a Hessian call with a remote method name. 
+  * eg. If the remote method has the following signature: public java.lang.Object echo(java.lang.Object value)
+  * simple pass @"echo" has the remote method name.
+  * @param aMethodName is an NSString with the name of the remote method for this call. 
+  * eg. If the remote method has the following signature: public java.lang.Object echo(java.lang.Object value)
+  * simple pass @"echo" has the remote method name.
+  */
 - (id) initWithRemoteMethodName:(NSString *) aMethodName;
 
+/** Return the data encoded in Hessian binary format.
+  * Call setParameters before calling data if the remote call will contain parameters.
+  * @return NSData with call encoded in Hessian binary format. 
+  * Call setParameters first if the call will contain parammeters.
+  */
 - (NSData *) data;
 
+/** Parameters that will be passed to the remote method.
+  * NSArray should contain the objects that will be passed. Ints, longs, doubles and bools
+  * should be passed as NSNumbers. The encoder will handle passing them as their instrinsic types
+  * to the remote service. eg [NSArray arrayWithObject:[NSNumber numberWithBool:NO]]; to pass a false bool.
+  * @param someParameters should be an NSArray that contains parameters for remote method. Ints, longs, doubles and bools
+  * should be passed as NSNumbers. The encoder will handle passing them as their instrinsic types
+  * to the remote service.
+  */
 - (void) setParameters: (NSArray *) someParameters;
 
+/** Return the method name the call was instantiated with.
+  * @return the method name the call was instantiated with.
+  */ 
 - (NSString*) methodName;
 
 @end
