8f51be1a624e118e04eacdfad48396b07e00e187
Byron Wright <bslayerw@gmail.com>
2006-03-10 00:08:35 +0000

    Added Doxygen documentation

--- BBSHessianDecoder.h
+++ BBSHessianDecoder.h
@@ -16,16 +16,31 @@
 // limitations under the License.
 //
 
+/** BBSHessianDecoder is responsible to decoding Hessian binary protocol encoded objects.
+  * If class mappings are specified those will also be used to instantiate to appropriate local class instance.
+  */
 @interface BBSHessianDecoder : NSObject {
-
+    @private
     NSInputStream * dataInputStream;
     NSMutableDictionary * classMapping;
 }
 
+/** Static method that decodes an object encoded in Hessian binary protocol and returns the object value.
+  * @param someData should be data encoded in Hessian binary format.
+  */
 + (id) decodedObjectWithData:(NSData *) someData;
 
+/** Instantiate and initialize the decoder for decoding with Hessian encoded data.
+  * @param someData is NSData that's encoded in Hessian binary format.
+  */
 - (id) initForReadingWithData:(NSData *) someData;
 
+/** Decodes and returns an object of the data that was encoded. 
+  * @return a decoded object. If the encoded data was an int,long, double or Bool
+  * then an NSNumber will be returned. Faults will be returned as an NSError. Object
+  * will be returned as NSDictionaries unless a mapping was available, if so then an attempt
+  * will be make to instantiate an instance of the local class.
+  */
 - (id) decodedObject;
 
 - (void) setClass:(Class) cls forClassName:(NSString *) codedName;
