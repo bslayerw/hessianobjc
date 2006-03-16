76884e559ac1ccf81b9123b29b968f06072c645c
Byron Wright <bslayerw@gmail.com>
2006-03-16 20:50:34 +0000

    added foundation header reference

--- BBSHessianInvocation.h
+++ BBSHessianInvocation.h
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-
+#import <Foundation/Foundation.h>
 @class BBSHessianCall;
 
 /** Encapsulates a HessianCall and the context for callback when the invocation is complete.
