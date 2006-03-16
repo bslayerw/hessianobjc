bbe8adbc83b38e4ef6856eefb099d318f4fb513a
Byron Wright <bslayerw@gmail.com>
2006-03-16 20:54:17 +0000

    added foundation header reference

--- BBSHessianEncoder.h
+++ BBSHessianEncoder.h
@@ -15,6 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+#import <Foundation/Foundation.h>
 /** Encodes objects into Hessian binary protocol format */
 
 @interface BBSHessianEncoder : NSCoder {
